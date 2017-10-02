3860b00d5d207374585d97158de0e15bee833873
5848 <5848@c04-imac6.ait-sydney.net.au>
2017-10-03 10:33:30 +1100

    Initial Commit

---
+++ Inclitiv3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Inclitiv3
+//
+//  Created by 5848 on 3/10/2017.
+//  Copyright © 2017 5848. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
