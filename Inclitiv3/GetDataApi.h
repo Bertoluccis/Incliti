b4f172d3f2c2a7a33afa9f0c8f4ba96bd87a1533
user129339 <user129339@103-246-99-72.moboware.com>
2017-10-26 20:50:27 +1100

    added new file GetDataApi

---
+++ Inclitiv3/GetDataApi.h
@@ -0,0 +1,14 @@
+//
+//  GetDataApi.h
+//  Inclitiv3
+//
+//  Created by user129339 on 10/26/17.
+//  Copyright © 2017 5848. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GetDataApi : NSObject
+
+
+@end
---
+++ Inclitiv3/GetDataApi.m
@@ -0,0 +1,13 @@
+//
+//  GetDataApi.m
+//  Inclitiv3
+//
+//  Created by user129339 on 10/26/17.
+//  Copyright © 2017 5848. All rights reserved.
+//
+
+#import "GetDataApi.h"
+
+@implementation GetDataApi
+
+@end
